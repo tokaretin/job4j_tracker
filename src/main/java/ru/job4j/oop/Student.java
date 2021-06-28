fa33565525fe1d3e54167de6d9167bc8222367df
tokaretin <tokaretin@gmail.com>
2021-06-28 21:37:00 +0300

    1.3. Вызов метода с аргументами. [#166086]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,13 +1,16 @@
 package ru.job4j.oop;
 
 public class Student {
-
     public void music() {
-        System.out.println("Tra - lya - lya");
+        System.out.println("Tra - lya - lya ");
     }
 
     public void song() {
-        System.out.println("I believe I can fly");
+        System.out.println("I can sign a song: ");
+    }
+
+    public void musicc(String lyrics) {
+        System.out.println("Tra - lya - lya " + lyrics);
     }
 
     public static void main(String[] args) {
@@ -15,8 +18,13 @@ public class Student {
         petya.music();
         petya.music();
         petya.music();
+        System.out.println();
         petya.song();
         petya.song();
         petya.song();
+        System.out.println();
+        String lyrics = "I believe, I can fly";
+        petya.musicc(lyrics);
+
     }
 }
