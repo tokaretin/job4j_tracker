960b82110117d26438cfb5729101f781d72d4dda
tokaretin <tokaretin@gmail.com>
2021-08-02 13:41:19 +0300

    2. Перегрузка конструктора [#173890]

---
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -0,0 +1,48 @@
+package ru.job4j.oop;
+
+public class Computer {
+
+    private boolean multiMonitor;
+
+    private int ssd;
+
+    private String cpu;
+
+    public Computer() {
+    }
+
+    public Computer(boolean multiMonitor, int ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu){
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
+    public void printInfo() {
+        System.out.println("Много мониторов: " + multiMonitor);
+        System.out.println("SSD: " + ssd);
+        System.out.println("Модель CPU: " + cpu);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Computer first = new Computer();
+        first.printInfo();
+        Computer second = new Computer(true, 500, "Intel Core I7-10700K");
+        second.printInfo();
+        Computer third = new Computer(500, "AMD Ryzen 5 3600");
+        third.printInfo();
+        Computer forth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        forth.printInfo();
+    }
+}
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,49 @@
 package ru.job4j.tracker;
 
 public class Item {
+
     private int id;
+
     private String name;
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
+    public Item(){
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void printInfo(){
+        System.out.println("ID: " + id);
+        System.out.println("Имя: " + name);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item("Андрей");
+        first.printInfo();
+        Item second = new Item(525, "Сергей");
+        second.printInfo();
     }
 }
